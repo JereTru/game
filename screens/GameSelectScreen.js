e95fd79abc80713489ef1940b40576bca83fd86b
VEIGA Rui <fveigarui@gmail.com>
2024-05-08 15:35:36 +0200

    Navigation vers tous les scren

--- screens/GameSelectScreen.js
+++ screens/GameSelectScreen.js
@@ -1,1 +1,78 @@
 // Écran de sélection du jeu où l'utilisateur peut choisir quel jeu jouer
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useNavigation } from '@react-navigation/native';
+import Button from '../components/ui/Button';
+
+const GameSelectScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+        <Button
+            title={"Go Choose a Room"}
+            onPress={() => navigation.navigate("HomeScreen")}
+        />
+
+        <Text style={styles.text}>
+            Sélection de Jeux 
+        </Text>
+
+        <Button
+            title={"BlackjackScreen"}
+            onPress={() => navigation.navigate("BlackjackScreen")}
+        />
+        <Button
+            title={"CardBattleScreen"}
+            onPress={() => navigation.navigate("CardBattleScreen")}
+        />
+        <Button
+            title={"ConnectFourScreen"}
+            onPress={() => navigation.navigate("ConnectFourScreen")}
+        />
+        <Button
+            title={"MemoryScreen"}
+            onPress={() => navigation.navigate("MemoryScreen")}
+        />
+        <Button
+            title={"MinesweeperScreen"}
+            onPress={() => navigation.navigate("MinesweeperScreen")}
+        />
+        <Button
+            title={"PokerScreen"}
+            onPress={() => navigation.navigate("PokerScreen")}
+        />
+        <Button
+            title={"RockPaperScissorsScreen"}
+            onPress={() => navigation.navigate("RockPaperScissorsScreen")}
+        />
+        <Button
+            title={"Snake"}
+            onPress={() => navigation.navigate("SnakeScreen")}
+        />
+        <Button
+            title={"TicTacToe (Morpion!)"}
+            onPress={() => navigation.navigate("TicTacToeScreen")}
+        />
+
+        <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+  }
+});
+
+export default GameSelectScreen;
--- screens/MemoryScreen.js
+++ screens/MemoryScreen.js
@@ -1,1 +1,41 @@
 // Écran spécifique pour le jeu
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Button from '../components/ui/Button';
+
+const MemoryScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+        <Text style={styles.text}>
+            Jeux Sélectionné Memory
+        </Text>
+        <Button
+            title={"<< Go Back"}
+            onPress={() => navigation.goBack()}
+            style={styles.buttonBack}
+        />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  buttonBack: {
+    backgroundColor: '#dd5555'
+  }
+});
+
+export default MemoryScreen;
